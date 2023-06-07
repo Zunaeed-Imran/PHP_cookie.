dcfd5c33d2e940ca7fc4cb77a75c0cb7f6c539fd
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-06-07 18:59:19 +0600

    cookie delete time example

---
+++ cookie2.php
@@ -0,0 +1,5 @@
+<?php
+  setcookie("fav_food", "pizza", time() - 0, "/");
+  setcookie("fav_drink", "coffee", time() - 0, "/");  
+  setcookie("fav_dessert", "ice cream", time() + (86400 * 4), "/");
+?>
